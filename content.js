16f324362ab31d9781e5207f464dcde7520a4521
lighscent <augustin.pro.prs@gmail.com>
2025-11-18 19:14:34 +0100

    Update button selector in content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 (function () {
-    const selector = 'button.ScCoreButton-sc-ocjdkq-0.fOtgyk';
-
+    const selector = 'button.ScCoreButton-sc-s7h2b7-0.kaIUar';
+    
     chrome.storage.local.get(['foundCount'], function (result) {
         if (typeof result.foundCount !== 'number') {
             chrome.storage.local.set({ foundCount: 0 });
